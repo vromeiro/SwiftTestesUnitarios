2841a2cbba47caa2d5762a6827671668f1878ac4
Vinicius Consulmagnos Romeiro <vinicius.romeiro@gmail.com>
2020-03-24 16:13:48 -0300

    aprendendo swift testes unitarios

---
+++ TestesUnitarios/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  TestesUnitarios
+//
+//  Created by Vinicius Romeiro on 01/02/17.
+//  Copyright © 2017 Vinicius Romeiro. All rights reserved.
+//
+
+import UIKit
+
+public class ViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    public override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    public static func sum(_ num1: Int, _ num2: Int) -> Int {
+        return num1 + num2;
+    }
+    
+    public static func sumAsync(_ num1: Int, _ num2: Int, completionHandler: @escaping (_ result: Int) -> Void) {
+        debugPrint("Iniciando soma");
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            completionHandler(self.sum(num1, num2));
+        }
+    }
+}
+
---
+++ TestesUnitariosTests/TestesUnitariosTests.swift
@@ -0,0 +1,49 @@
+//
+//  TestesUnitariosTests.swift
+//  TestesUnitariosTests
+//
+//  Created by Vinicius Romeiro on 01/02/17.
+//  Copyright © 2017 Vinicius Romeiro. All rights reserved.
+//
+
+import XCTest
+@testable import TestesUnitarios
+
+class TestesUnitariosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testSum() {
+        let result = ViewController.sum(1, 2);
+        XCTAssert(result == 4, "Valor errado da soma");
+    }
+    
+    func testPerformanceExample() {
+        self.measure {
+            _ = ViewController.sum(1, 2);
+        }
+    }
+    
+    func testSumAsync() {
+        let asyncExpectation = self.expectation(description: "sum");
+        
+        ViewController.sumAsync(1, 2, completionHandler: { result in
+            asyncExpectation.fulfill();
+            
+            XCTAssert(result == 3);
+        });
+        
+        self.waitForExpectations(timeout: 5) { error in
+            XCTAssertNil(error, "Algo deu errado depois de 5 segundos");
+        }
+    }
+    
+}
